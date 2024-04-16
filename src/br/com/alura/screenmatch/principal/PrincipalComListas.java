77476e984cb9e48552707e5f1684c059804c68de
Arthur Barbosa de Morais <bmartdomeu@gmail.com>
2024-04-16 14:19:26 -0300

    ordenação de listas

--- src/br/com/alura/screenmatch/principal/PrincipalComListas.java
+++ src/br/com/alura/screenmatch/principal/PrincipalComListas.java
@@ -4,7 +4,7 @@ import br.com.alura.screenmatch.modelos.Filme;
 import br.com.alura.screenmatch.modelos.Serie;
 import br.com.alura.screenmatch.modelos.Titulo;
 
-import java.util.ArrayList;
+import java.util.*;
 
 public class PrincipalComListas {
     public static void main(String[] args) {
@@ -27,8 +27,22 @@ public class PrincipalComListas {
             if (item instanceof Filme filme && filme.getClassificacao() > 2) {
                 System.out.println("Classificação: " + filme.getClassificacao());
             }
-
-            
         }
+
+        List<String> buscaPorArtista = new LinkedList<>();
+        buscaPorArtista.add("Adam Sandler");
+        buscaPorArtista.add("Arthur");
+        buscaPorArtista.add("Cacau");
+        System.out.println(buscaPorArtista);
+
+        Collections.sort(buscaPorArtista);
+        System.out.println("Depois da ordenacao");
+        System.out.println(buscaPorArtista);
+        System.out.println("Lista de títulos ordenados");
+        Collections.sort(lista);
+        System.out.println(lista);
+        lista.sort(Comparator.comparing(Titulo::getAnoDeLancamento));
+        System.out.println("Ordenando por ano");
+        System.out.println(lista);
     }
 }
